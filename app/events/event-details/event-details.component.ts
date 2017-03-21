1e6664748964f1a895166f7c41d44726c7a92dc5
Trieu Pham <chautrieu@gmail.com>
2017-03-22 06:12:03 +0700

    create 1st route

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -2,7 +2,7 @@ import {OnInit, Component} from '@angular/core'
 import {EventService} from '../shared/event.service'
 
 @Component({
-  templateUrl: './event-details.component.html',
+  templateUrl: 'app/events/event-details/event-details.component.html',
   styles: [`
     .container { padding-left: 20px; padding-right: 20px }
     .event-image { height: 100px }
