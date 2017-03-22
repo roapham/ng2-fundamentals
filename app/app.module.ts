6bb6f71b222530a5edc9ddf46142a82c5ed6c65e
Trieu Pham <chautrieu@gmail.com>
2017-03-23 05:50:56 +0700

    pre-load data

--- app/app.module.ts
+++ app/app.module.ts
@@ -12,6 +12,7 @@ import { EventDetailsComponent } from './events/event-details/event-details.comp
 import { CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './errors/404.component'
 import { EventRouteActivator } from './events/event-details/event-route-activator.service'
+import { EventListResolver } from './events/events-list-resolver.service'
 
 import { appRoutes } from './routes'
 
@@ -32,6 +33,7 @@ import { appRoutes } from './routes'
     EventService,
     ToastrService,
     EventRouteActivator,
+    EventListResolver,
     {
       provide: 'canDeactivateCreateEvent',
       useValue: checkDirtyState
