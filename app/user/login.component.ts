03da460614fb83e8d0aa7722cda1c44f2c2d9cc4
Trieu Pham <chautrieu@gmail.com>
2017-03-23 11:23:07 +0700

    validate template-based form

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -4,12 +4,15 @@ import { Router } from "@angular/router";
 
 @Component({
     moduleId: module.id,
-    templateUrl: 'login.component.html'
+    templateUrl: 'login.component.html',
+    styles: [`
+        em { float: right; color: #E05C65; padding-left:10px } 
+    `]
 })
 export class LoginComponent implements OnInit {
     constructor(
-        private authService:AuthService,
-        private router:Router
+        private authService: AuthService,
+        private router: Router
     ) { }
 
     ngOnInit() { }
